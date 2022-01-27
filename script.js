a0f331d66c22650b3ef94e49000301e635e621e7
Oleg Kravchenko <kitaist@kitaist.com>
2022-01-28 05:12:51 +0800

    Lesson 1, mandatory homework

---
+++ script.js
@@ -0,0 +1,13 @@
+var title = 'old way'
+
+const screens = 'constant, list of items'
+
+let screenPrice = 'changable, 425'
+
+let rollback, fullPrice, adaptive
+
+alert('Hi there!')
+
+console.log('Hello Console!')
+
+// В репозитории создать новую ветку lesson01, выполнить commit в новую ветку и push
