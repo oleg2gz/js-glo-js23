206220641e5e24b891c28467a36239ccc0523fc0
Oleg Kravchenko <kitaist@kitaist.com>
2022-01-31 18:31:07 +0800

    Lesson 3, mandatory homework

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 'use strict'
 
-const title = 'Проект'
-const screens = 'Простые, Сложные, Интерактивные'
-const screenPrice = 425
+const title = prompt('Как называется ваш проект?', 'Проект')
+const screens = prompt(
+  'Какие типы экранов нужно разработать?',
+  'Простые, Сложные, Интерактивные'
+)
+const screenPrice = +prompt('Сколько будет стоить данная работа?')
+const adaptive = confirm('Нужен ли адаптив на сайте?')
+const service1 = prompt('Какой дополнительный тип услуги нужен?')
+const servicePrice1 = +prompt('Сколько это будет стоить?')
+const service2 = prompt('Какой дополнительный тип услуги нужен?')
+const servicePrice2 = +prompt('Сколько это будет стоить?')
+const fullPrice = screenPrice + servicePrice1 + servicePrice2
 const rollback = 15
-const fullPrice = 5000
-const adaptive = true
+const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100))
 
-// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive
+console.log(servicePercentPrice)
+
+switch (true) {
+  case fullPrice >= 30000:
+    console.log('Даем скидку в 10%')
+    break
+  case 15000 <= fullPrice && fullPrice < 30000:
+    console.log('Даем скидку в 5%')
+    break
+  case 0 <= fullPrice && fullPrice < 15000:
+    console.log('Скидка не предусмотрена')
+    break
+  default:
+    console.warn('Что то пошло не так')
+}
+
+// Previous functionality
+
 console.log(typeof title)
 console.log(typeof fullPrice)
 console.log(typeof adaptive)
 
-// Вывести в консоль длину строки screens
-console.log(screens.length)
+console.log(screens?.length || 'no data')
 
-// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
 console.log(
   `Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юаней`
 )
@@ -23,8 +46,6 @@ console.log(
   `Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юаней`
 )
 
-// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
-console.log(screens.toLowerCase().split(', '))
+console.log(screens?.toLowerCase().split(', ') || 'no data')
 
-// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
 console.log(fullPrice * (rollback / 100))
