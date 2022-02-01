d910866f820d91b4a1a29cbff1bb205682f75f9b
Oleg Kravchenko <kitaist@kitaist.com>
2022-02-01 20:47:50 +0800

    Lesson 4, mandatory homework

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const title = prompt('Как называется ваш проект?', 'Проект')
-const screens = prompt(
+let title = prompt('Как называется ваш проект?', 'Проект')
+let screens = prompt(
   'Какие типы экранов нужно разработать?',
   'Простые, Сложные, Интерактивные'
 )
@@ -11,41 +11,69 @@ const service1 = prompt('Какой дополнительный тип услу
 const servicePrice1 = +prompt('Сколько это будет стоить?')
 const service2 = prompt('Какой дополнительный тип услуги нужен?')
 const servicePrice2 = +prompt('Сколько это будет стоить?')
-const fullPrice = screenPrice + servicePrice1 + servicePrice2
 const rollback = 15
-const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100))
 
-console.log(servicePercentPrice)
+let allServicePrices, fullPrice, servicePercentPrice
 
-switch (true) {
-  case fullPrice >= 30000:
-    console.log('Даем скидку в 10%')
-    break
-  case 15000 <= fullPrice && fullPrice < 30000:
-    console.log('Даем скидку в 5%')
-    break
-  case 0 <= fullPrice && fullPrice < 15000:
-    console.log('Скидка не предусмотрена')
-    break
-  default:
-    console.warn('Что то пошло не так')
+const getAllServicePrices = function () {
+  return servicePrice1 + servicePrice2
 }
 
-// Previous functionality
+function getFullPrice() {
+  return screenPrice + allServicePrices
+}
 
-console.log(typeof title)
-console.log(typeof fullPrice)
-console.log(typeof adaptive)
+const getTitle = (str) => {
+  str = str || 'проект'
+  str = str.trim()
 
-console.log(screens?.length || 'no data')
+  return str[0].toUpperCase() + str.slice(1).toLowerCase()
+}
 
-console.log(
-  `Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юаней`
-)
-console.log(
-  `Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юаней`
-)
+const getServicePercentPrices = () => {
+  return Math.ceil(fullPrice - fullPrice * (rollback / 100))
+}
 
-console.log(screens?.toLowerCase().split(', ') || 'no data')
+const getRollbackMessage = (price) => {
+  switch (true) {
+    case price >= 30000:
+      return 'Даем скидку в 10%'
+    case 15000 <= price && price < 30000:
+      return 'Даем скидку в 5%'
+    case 0 <= price && price < 15000:
+      return 'Скидка не предусмотрена'
+    default:
+      return 'Что то пошло не так'
+  }
+}
 
-console.log(fullPrice * (rollback / 100))
+const showTypeOf = (...types) => {
+  types.forEach((item) => console.log(item, typeof item))
+}
+
+// TMP Временно, удаление лишних запятых в конце строки
+const getScreens = (str) => {
+  str = str || ''
+  str = str.trim()
+
+  if (str[str.length - 1] === ',') {
+    str = str.slice(0, -1)
+  }
+  return str
+}
+
+screens = getScreens(screens)
+
+title = getTitle(title)
+allServicePrices = getAllServicePrices()
+fullPrice = getFullPrice()
+servicePercentPrice = getServicePercentPrices()
+
+showTypeOf(title, fullPrice, adaptive)
+
+console.log(getRollbackMessage(fullPrice))
+
+console.log('screens as a string: ', screens?.toLowerCase() || 'no data')
+console.log('screens as an array: ', screens?.toLowerCase().split(', '))
+
+console.log(getServicePercentPrices())
